50b77c7b95a51b05b0ec0db36b6ac90aba360b0e
7Romain <osiiriis@ymail.com>
2022-06-09 23:37:10 +0200

    avec des commentaires c'est mieux

--- src/Outils.java
+++ src/Outils.java
@@ -1,5 +1,17 @@
 public class Outils {
 
+    /**
+     * @author 7Romain
+     * @version 1
+     */
+
+    /**
+     * @param annee
+     * @return boolean
+     *         Fonction qui renvoie true si l'année entrée en paramètre est une
+     *         année bissextile,
+     *         sinon renvoie false.
+     */
     public static boolean isBissextile(int annee) {
         if (Math.floorMod(annee, 4) != 0)
             return false;
@@ -13,37 +25,77 @@ public class Outils {
 
     }
 
+    /**
+     * @param numerateur
+     * @param denominateur
+     * @return boolean
+     *         fonction pour factoriser modulo, entrez un numérateur et le
+     *         dénominateur et vous obtientrais true
+     *         si le numérateur est divisible par le dénominateur , sinon renvoie
+     *         false.
+     */
     public static boolean mod(int numerateur, int denominateur) {
         return (Math.floorMod(numerateur, denominateur) == 0);
 
     }
 
+    /**
+     * @param annee
+     * @return boolean
+     *         * Fonction raccourcie qui renvoie true si l'année entrée en paramètre
+     *         est une année bissextile,
+     *         sinon renvoie false.
+     */
     public static boolean shortBiss(int annee) {
         return ((mod(annee, 4) && !mod(annee, 100)) ||
                 (mod(annee, 4) && mod(annee, 100) && mod(annee, 400)));
 
     }
 
+    /**
+     * @param mois
+     * @return boolean
+     *         Vérifie si mois est entre 1 et 12.
+     */
     public static boolean verifierMois(int mois) {
         return (0 < mois && mois <= 12);
 
     }
 
+    /**
+     * @param annee
+     * @return boolean
+     *         Vérifie si année est entre 0 et 9999.
+     */
     public static boolean verifierAnnee(int annee) {
         return (0 <= annee && annee <= 9999);
 
     }
 
+    /**
+     * @param jour
+     * @return boolean
+     *         Vérifie si jour est entre 1 et 31.
+     */
     public static boolean verifierJour(int jour) {
 
         return !(1 > jour || jour > 31);
     }
 
+    /**
+     * @param jour
+     * @param mois
+     * @param annee
+     * @return boolean
+     *         vérifie s'il s'agit ou non d'une date valide.
+     *         revoie true si oui ; sinon renvoie false.
+     * 
+     */
     public static boolean verifierDate(int jour, int mois, int annee) {
         if (verifierJour(jour) && verifierMois(mois) && verifierAnnee(annee)) {
-            if (isBissextile(annee)) {
-                // switch mois 31 jours, mois 30 jours, mois 29jours
-                switch (mois) {
+            if (isBissextile(annee)) { // année bissextile
+
+                switch (mois) {// on a déjà vérifié que jour est entre 1 et 31
                     case 1:
                     case 3:
                     case 5:
@@ -57,14 +109,15 @@ public class Outils {
                     case 6:
                     case 9:
                     case 11:
-                        return (1 <= jour && jour <= 30);
+                        return (1 <= jour && jour <= 30); // les mois Avril Juin Septembre et Novembre n'ont que 30
+                                                          // jours
 
-                    case 2:
+                    case 2: // mois de février en année bissextile 29 jours
                         return (1 <= jour && jour <= 29);
 
                 }
 
-            } else {
+            } else { // presque pareill en année non bissextile
 
                 switch (mois) {
                     case 1:
@@ -82,7 +135,7 @@ public class Outils {
                     case 11:
                         return (1 <= jour && jour <= 30);
 
-                    case 2:
+                    case 2: // sauf que le mois de février à 28 jours
                         return (1 <= jour && jour <= 28);
 
                 }
